5a61a44c058fbc085efd6f803a3f71503adf37e6
happyZYC <zhaoyongchang_life@163.com>
2017-05-23 23:17:05 +0800

    添加项目
    向GitHub添加项目

---
+++ ImageClipsView/ImageClipsView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageClipsView
+//
+//  Created by 赵永昌 on 2017/5/23.
+//  Copyright © 2017年 chengzhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
