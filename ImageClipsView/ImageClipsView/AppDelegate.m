c7304b0644e86c006eed38604a64aa3b65782d69
happyZYC <zhaoyongchang_life@163.com>
2017-11-23 15:10:37 +0800

    测试分支11

--- ImageClipsView/ImageClipsView/AppDelegate.m
+++ ImageClipsView/ImageClipsView/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"测试1111111111");
     return YES;
 }
 
